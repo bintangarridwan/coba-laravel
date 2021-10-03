8ec5b5fe9556929bd72dbd6453e4e5a4d79a034d
bintangarridwan <bintang.arridwan8@gmail.com>
2021-10-03 21:23:14 +0700

    materi 7 lanjutan

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,11 +15,11 @@ class PostController extends Controller
         ]);
     }
 
-    public function show($slug)
+    public function show(Post $post)
     {
         return view('post', [
             "title" => "Single Post",
-            "posts" => Post::find($slug)
+            "posts" => $post
         ]);
     }
 }
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,7 +3,7 @@
     @foreach ($posts as $post)
     <article class="mb-5">
         <h2> 
-        <a href="/blog/{{ $post->id }}">{{ $post->title }}</a>
+        <a href="/blog/{{ $post->slug }}">{{ $post->title }}</a>
         </h2>
         <p>{{ $post->excerpt }}</p>
     </article>
