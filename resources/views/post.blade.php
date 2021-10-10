6c0371f40aa29902e4bacc6341a45591516ad29b
bintangarridwan <bintang.arridwan8@gmail.com>
2021-10-10 18:48:05 +0700

    post category n eloquent relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,8 +2,10 @@
 
 @section('container')
     <article>
-        <h2>{{ $posts->title }}</h2>
-        <h5>{{ $posts->author }}</h5>
+        <h2 class="mb-3">{{ $posts->title }}</h2>
+
+        <p class="mb-4">By : Bintang Rajamuda in <a href="/categories/{{ $posts->category->slug }}">{{ $posts->category->name }}</a></p>
+
         {!! $posts->body !!}
     </article>
     <a href="/blog">Back To Post</a>
